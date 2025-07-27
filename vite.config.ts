8ca511caa03d0701ff21eb7b57c6d49d26c724b3
rohansharma2410 <sharmar2410@gmail.com>
2025-07-27 17:15:44 +0530

    vhnages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from 'path';
 
 export default defineConfig({
+  base: '/', // Use '/' for root deployment or when using a custom domain
   server: {
     host: "::",
     port: 8080,
@@ -16,6 +17,3 @@ export default defineConfig({
     },
   },
 });
-
-
-
